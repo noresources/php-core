762d538ac688d7edb60f7c131b61f3a490efc4e1
Renaud Guillard <dev@nore.fr>
2015-10-19 10:56:58 +0200

    doc

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 ns-php/core
 ===========
 A set of basic classes and functions for PHP
+
+# Usage
+```php
+include_once ('path/to/ns-php-core/core.inc.php');
+```
+ns-php-core uses the PHP autoload feature to automatically load class files. 
+Files containing function definitions have to be included manually
+
+# File name policy
+
+* A file name starting with a upper case letter contains class definition(s)
+* A file name starting with a small case letter contains function definitions
+* A file with the extension .inc.php must be included using `include`/`include_once`
+* A file with the extension .php should use `require`/`require_once`
--- core.inc.php
+++ core.inc.php
@@ -15,15 +15,38 @@ use \InvalidArgumentException;
 
 if (!defined('NS_PHP_PATH'))
 {
+	/**
+	 *
+	 * @var string Path of the root directory of all NoreSources modules
+	 */
 	define('NS_PHP_PATH', realpath(__DIR__ . '/..'));
 }
 
 if (!defined('NS_PHP_CORE_PATH'))
 {
+	/**
+	 *
+	 * @var string Path of the NoreSources core source tree
+	 */
 	define('NS_PHP_CORE_PATH', realpath(__DIR__));
 }
+
+/**
+ * NoreSources core major version number
+ * @var integer [0-99]
+ */
 const VERSION_MAJOR = 0;
-const VERSION_MINOR = 1;
+
+/**
+ * NoreSources core minor version number
+ * @var integer [0-99]
+ */
+const VERSION_MINOR = 2;
+
+/**
+ * NoreSources core patch version number
+ * @var integer [0-99]
+ */
 const VERSION_PATCH = 0;
 
 /**
@@ -37,53 +60,13 @@ function version_string()
 	return (VERSION_MAJOR . '.' . VERSION_MINOR . '.' . VERSION_PATCH);
 }
 
+/**
+ * Version number of the NoreSources core module
+ * @return integer
+ */
 function version_number()
 {
 	return (VERSION_MAJOR * 10000 + VERSION_MINOR * 100 + VERSION_PATCH);
-}
-
-/**
- * Load a ns-php module file
- *
- * @param $resourceName Source
- *        	file short name.
- *        	$resourceName is the module file path without file extension
- *        	and relative to the path defined by NS_PHP_PATH
- *        	
- * @param boolean $silent
- *        	If <code>true</code>, no exception will be raised if the resource can't be found
- *        	
- *        	If the resourceName was loaded and a file named
- *        	NS_PHP_PATH . '/' . @param $resourceName . '.inc.php' exists, then
- *        	this file will be included (<code>include_once</code>)
- *        	
- * @example load('core/strings') will call require_once (<path to ns-php root>/core/strings.php)
- *         
- * @return <code>true</code> if the file can be loaded.
- *         <code>false</code> If the file can't be loaded and @param $silent is set to <code>true</code>.
- *         If @param $silent is <code>false</code> and the file can't be loaded, a InvalidArgumentException exception is raised.
- */
-function load($resourceName, $silent = false)
-{
-	$fileName = NS_PHP_PATH . '/' . $resourceName . '.php';
-	$exists = file_exists($fileName);
-	
-	if ($exists)
-	{
-		require_once ($fileName);
-	}
-	else if (!$silent)
-	{
-		throw new InvalidArgumentException($resourceName . ' not found');
-	}
-	
-	$fileName = NS_PHP_PATH . '/' . $resourceName . '.inc.php';
-	if (file_exists($fileName))
-	{
-		include_once ($fileName);
-	}
-	
-	return $exists;
 }
 
 include_once (NS_PHP_CORE_PATH . '/core.autoload.inc.php');
