4e28a556f32057b21c081c468e7404b5e06cce85
Renaud Guillard <dev@nore.fr>
2019-07-02 20:55:18 +0200

    TypeDescription

---
+++ TypeDescription.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace NoreSources;
+
+class TypeDescription
+{
+
+	public static function getName($element)
+	{
+		if (\is_object($element))
+		{
+			return get_class($element);
+		}
+		return gettype($element);
+	}
+}
