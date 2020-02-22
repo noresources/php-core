d9e8cee4a475480fc61eb546b9919545d5f16210
Renaud Guillard <dev@nore.fr>
2020-02-22 22:22:24 +0100

    sami configuration update

--- resources/sami/config.php
+++ resources/sami/config.php
@@ -1,6 +1,10 @@
 <?php
+/**
+ * You are supposed to run sami in the directory where you want 
+ * to output the API documentation 
+ */
 return new Sami\Sami(__DIR__ . '/../../src',
 	[
 		'title' => 'NoreSource Core',
-		'build_dir' => './doc'
+		'build_dir' => 'api'
 	]);
