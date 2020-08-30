5fe99b5134376113d127a8db3f8edbe8b6d8234f
Renaud Guillard <dev@nore.fr>
2020-08-30 17:08:22 +0200

    CascadedValueTree container

---
+++ src/CascadedValueTree.php
@@ -0,0 +1,222 @@
+<?php
+
+/**
+ * Copyright © 2020 by Renaud Guillard (dev@nore.fr)
+ * Distributed under the terms of the MIT License, see LICENSE
+ */
+namespace NoreSources;
+
+/**
+ * Represents a tree of values
+ * where undefined tree path will take their value
+ * from the nearest valid ancestor value.
+ */
+class CascadedValueTree implements \ArrayAccess, ArrayRepresentation
+{
+
+	/**
+	 *
+	 * @param string|array $query
+	 *        	Key path
+	 * @param mixed $dflt
+	 *        	Default value
+	 * @return mixed Value associated to the leaf key of $query.
+	 *         If the value does not exists, the value of the leaf key of the
+	 *         nearest ancestor is returned.
+	 *         Otherwise, return the default value
+	 */
+	public function query($query, $dflt = null)
+	{
+		$value = $dflt;
+
+		$this->walk($query,
+			function ($node, $leafKey) use (&$value) {
+				$value = $this->keyValue($node, $leafKey, $value);
+				return true;
+			},
+			function ($node, $key, $leafKey) {
+				return $this->hasNode($node, $key);
+			},
+			function ($node, $key, $leafKey) use (&$value) {
+				$node = $node[self::NODES][$key];
+				$value = $this->keyValue($node, $leafKey, $value);
+				return true;
+			});
+
+		return $value;
+	}
+
+	public function getArrayCopy()
+	{
+		return $this->valueTree->getArrayCopy();
+	}
+
+	/**
+	 * Query key path value
+	 *
+	 * @param string|array $query
+	 *        	Key path
+	 * @return mixed
+	 */
+	public function offsetGet($query)
+	{
+		return $this->query($query);
+	}
+
+	/**
+	 * Set the value associated to the given key path
+	 *
+	 * @param string|array $query
+	 *        	Key path
+	 * @param mixed $value
+	 *        	Value
+	 */
+	public function offsetSet($query, $value)
+	{
+		return $this->walk($query, null, null, null,
+			function ($node, $valid) use ($value) {
+				Container::setValue($node, self::VALUE, $value);
+				return true;
+			});
+	}
+
+	/**
+	 * Indicates if a value is associated to the given key path
+	 *
+	 * @param string|array $query
+	 *        	Key path
+	 * @return boolean
+	 */
+	public function offsetExists($query)
+	{
+		return $this->walk($query, null,
+			function ($node, $key, $leafKey) {
+				return $this->hasNode($node, $key);
+			}, null,
+			function ($node, $valid) {
+				return $valid && Container::keyExists($node, self::VALUE);
+			});
+	}
+
+	/**
+	 * Remove the value associated to the given key path
+	 *
+	 * @param string|array $query
+	 *        	Key path
+	 * @return true if value was removed
+	 */
+	public function offsetUnset($query)
+	{
+		return $this->walk($query, null,
+			function ($node, $key) {
+				$valid = $node->offsetExists(self::NODES) &&
+				$node[self::NODES]->offsetExists($key);
+
+				return $valid;
+			}, null,
+			function ($node, $valid) {
+				if (!$valid)
+					return $valid;
+				if (!$node->offsetExists(self::VALUE))
+					return false;
+				$node->offsetUnset(self::VALUE);
+				return true;
+			});
+	}
+
+	/**
+	 * Alias of query() method
+	 *
+	 * @return mixed
+	 */
+	public function __invoke()
+	{
+		return \call_user_func_array([
+			$this,
+			'query'
+		], func_get_args());
+	}
+
+	/**
+	 * Set the separator of the string-form key path query
+	 *
+	 * @param string $separator
+	 */
+	public function setKeySeparator($separator)
+	{
+		$this->keySeparator = $separator;
+	}
+
+	public function __construct()
+	{
+		$this->setKeySeparator('.');
+		$this->valueTree = new \ArrayObject();
+	}
+
+	private function hasNode(\ArrayObject $node, $key)
+	{
+		return $node->offsetExists(self::NODES) &&
+			$node[self::NODES]->offsetExists($key);
+	}
+
+	private function keyValue(\ArrayObject $node, $key, $dflt = null)
+	{
+		if ($node->offsetExists(self::NODES) &&
+			$node[self::NODES]->offsetExists($key))
+			return Container::keyValue($node[self::NODES][$key],
+				self::VALUE, $dflt);
+		return $dflt;
+	}
+
+	private function walk($query, $init = null, $nodePreprocess = null,
+		$nodeProcess = null, $completion = null)
+	{
+		if (!\is_array($query))
+			$query = \explode($this->keySeparator, $query);
+
+		$node = $this->valueTree;
+		$c = \count($query);
+		$valid = true;
+		$leafKey = ($c ? $query[$c - 1] : null);
+
+		if (\is_callable($init))
+			$valid = \call_user_func($init, $node, $leafKey);
+
+		if ($valid)
+			foreach ($query as $key)
+			{
+				if (\is_callable($nodePreprocess))
+					$valid = \call_user_func($nodePreprocess, $node,
+						$key, $leafKey);
+
+				if (!$valid)
+					break;
+
+				if (!$node->offsetExists(self::NODES))
+					$node[self::NODES] = new \ArrayObject();
+				if (!$node[self::NODES]->offsetExists($key))
+					$node[self::NODES][$key] = new \ArrayObject();
+
+				if (\is_callable($nodeProcess))
+					if (!($valid = \call_user_func($nodeProcess, $node,
+						$key, $leafKey)))
+						break;
+
+				$node = $node[self::NODES][$key];
+			}
+
+		if (\is_callable($completion))
+			$valid = \call_user_func($completion, $node, $valid);
+
+		return $valid;
+	}
+
+	const NODES = 'nodes';
+
+	const VALUE = 'value';
+
+	private $keySeparator;
+
+	private $valueTree;
+}
+
---
+++ tests/CascadedValueTreeTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright © 2012 - 2020 by Renaud Guillard (dev@nore.fr)
+ * Distributed under the terms of the MIT License, see LICENSE
+ */
+namespace NoreSources;
+
+final class CascadedValueTreeTest extends \PHPUnit\Framework\TestCase
+{
+
+	public function testCVT()
+	{
+		$c = new CascadedValueTree();
+		$this->assertInstanceOf(CascadedValueTree::class, $c);
+
+		$c->offsetSet('foo', 'foo value');
+		$c->offsetSet('foo.bar', 'bar value in foo');
+		$c->offsetSet('foo.baz', 'baz value in foo');
+		$c['foo.bar.baz'] = 'baz';
+
+		$this->assertArrayHasKey('foo.bar.baz', $c);
+		$this->assertArrayHasKey('foo.baz', $c);
+		$this->assertArrayNotHasKey('baz', $c);
+
+		$this->assertEquals('foo value', $c['foo'], 'Value of "foo"');
+		$this->assertEquals('baz', $c['foo.bar.baz'],
+			'Value of "foo.bar.baz"');
+		$this->assertEquals('baz value in foo', $c['foo.undef.baz'],
+			'Value of "foo.undef.baz"' . PHP_EOL . $this->toJson($c));
+
+		$removed = $c->offsetUnset('foo.bar.baz');
+		$this->assertTrue($removed,
+			'fom.bar.baz removed' . PHP_EOL . $this->toJson($c));
+		$this->assertArrayNotHasKey('foo.bar.baz', $c,
+			'After removal' . PHP_EOL . $this->toJson($c));
+	}
+
+	private function toJson(CascadedValueTree $c)
+	{
+		return \json_encode($c->getArrayCopy(), JSON_PRETTY_PRINT);
+	}
+}
